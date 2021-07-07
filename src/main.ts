af4546f7f7843848aabf5746f864b4fa18bd72c5
Rominnar <43a022@gmail.com>
2021-07-07 12:51:37 +0800

    feat: element-plus引入

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,11 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+//elementui 
+import elemenCreator from './plugins/element'
+
+const app = createApp(App);
+app.use(store).use(router).mount('#app');
+
+//elementui use
+elemenCreator(app);
---
+++ src/plugins/element.ts
@@ -0,0 +1,44 @@
+import {
+  ElTable,
+  ElTableColumn,
+  ElInfiniteScroll,
+  ElLoading,
+  ElMessage,
+  ElMessageBox,
+  ElNotification,
+  ElButton,
+  ElMenu,
+  ElMenuItem,
+  ElMenuItemGroup,
+  ElSubmenu,
+  ElIcon,
+  ElContainer,
+  ElAside,
+  ElHeader,
+} from 'element-plus';
+import { App } from "vue";
+
+const components = [
+  ElTable,
+  ElTableColumn,
+  ElButton,
+  ElMenu,
+  ElMenuItem,
+  ElMenuItemGroup,
+  ElSubmenu,
+  ElIcon,
+  ElContainer,
+  ElAside,
+  ElHeader,
+];
+
+const plugins = [ElInfiniteScroll, ElLoading, ElMessage, ElMessageBox, ElNotification];
+
+export default function elementCreator(app: App):void {
+  components.forEach((component) => {
+    app.component(component.name, component);
+  });
+  plugins.forEach((plugin) => {
+    app.use(plugin);
+  });
+}
